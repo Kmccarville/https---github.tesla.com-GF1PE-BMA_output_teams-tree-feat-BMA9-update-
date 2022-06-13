9b046598716629efa2bc8a326b2e211a7c8c8b40
Michael Berlied <mberlied@tesla.com>
2022-06-13 09:37:55 -0700

    update error handler email

--- app/error_handler.py
+++ app/error_handler.py
@@ -27,7 +27,7 @@ def e_handler(e, trace=None, subject=None, handleType=0):
     </body>
     </html>
     '''
-    payload={"text": "Error Exception", "html":html, "recipients":"mfouad@tesla.com", "cc":"mfouad@tesla.com", "Bcc":"mfouad@tesla.com"}
+    payload={"text": "Error Exception", "html":html, "recipients":"mberlied@tesla.com", "cc":"mberlied@tesla.com", "Bcc":"mberlied@tesla.com"}
     subject = subject if subject is not None else "BMA Teams Poster Error Exception"
 
     if debug:
