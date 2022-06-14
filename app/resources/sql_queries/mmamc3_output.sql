ed06ceb445427bf7d1fffb488f3593ab97853851
Michael Berlied <mberlied@tesla.com>
2022-06-14 09:48:16 -0700

    fix mmamc3 query variables

--- app/resources/sql_queries/mmamc3_output.sql
+++ app/resources/sql_queries/mmamc3_output.sql
@@ -1,2 +1,2 @@
     SELECT count(distinct tp.thingid)/4 FROM thingpath tp
-    WHERE tp.flowstepname = 'MBM-25000' AND tp.exitcompletioncode = 'PASS' AND tp.completed between '{start}' and '{end}'
+    WHERE tp.flowstepname = 'MBM-25000' AND tp.exitcompletioncode = 'PASS' AND tp.completed between '{start_time}' and '{end_time}'
