c7764af6ee657f78de106c8c6db3af06d32b2668
Michael Berlied <mberlied@tesla.com>
2022-06-14 11:49:45 -0700

    add env to main

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ if __name__ == '__main__':
         logging.info("Code is running...better go catch it!")
         logging.info("Environment: %s", env)
 
-        schedule.every().hour.at(":00").do(output123)
+        schedule.every().hour.at(":00").do(output123(env))
         # schedule.every().hour.at(":01").do(output45)
         # schedule.every().hour.at(":02").do(outputz3)
         # schedule.every().hour.at(":03").do(outputz4)
