56f59fdec19fcd26e216f8e0ec6fce946a4100f8
Michael Berlied <mberlied@tesla.com>
2022-06-14 08:27:57 -0700

    update file_read for explicit read only

--- app/common/helper_functions.py
+++ app/common/helper_functions.py
@@ -3,6 +3,6 @@ import os
 from test import debug as masterdebug
 
 def file_reader(FilePath):
-    with open(FilePath) as f:
+    with open(FilePath,"r") as f:
         contents = f.read()
         return contents
