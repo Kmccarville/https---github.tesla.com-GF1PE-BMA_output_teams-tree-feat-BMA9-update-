5a5ea06a6c7e23447281c507b03bd2ec4a6bcc0c
Marion Quien <mquien@tesla.com>
2022-12-01 11:46:50 -0800

    general clean-up in zone3 output

--- app/resources/outputz3.py
+++ app/resources/outputz3.py
@@ -12,73 +12,6 @@ from resources import z3_wb_teep
 
 warnings.filterwarnings("ignore")
 
-#bonder ideal ct
-def query_ideal_ct_data(db):
-    query = """
-            SELECT PRODUCTION_LINE AS LINE,UUT_MODEL,IDEAL_CYCLE_TIME/60 AS CT FROM m3_teep.ideal_cycle_times
-            WHERE MACHINE_TYPE='BONDER'
-            AND REVISION=(SELECT MAX(REVISION) FROM m3_teep.ideal_cycle_times WHERE MACHINE_TYPE='BONDER')
-            """
-    ct_df = pd.read_sql(query,db)
-    return ct_df
-    
-#pull bonder actual and ideal cycle times
-def query_bonder_ct(db,start,end, ideal_ct_df, line_list):
-
-    def get_ideal_ct(ideal_ct_df,line,model):
-        ct_query = ideal_ct_df.query(f"LINE=='{line}' and UUT_MODEL=='{model}'",engine='python')
-        if len(ct_query):
-            ct_ideal = ct_query.iloc[0]['CT']
-        else:
-            ct_ideal = 0
-        return ct_ideal
-        
-    ct_query = f"""
-    SELECT
-    left(a.name,4) as LINE,
-    CASE
-    WHEN p.partnumber in ('1091593-01-H','1609293-01-A','1609293-02-A') THEN 'E3S'
-    WHEN p.partnumber in ('1091594-01-H','1609294-01-A','1609294-02-A') THEN 'E3L'
-    WHEN p.partnumber in ('1609905-01-A','1609906-01-A','1609906-02-A') THEN 'E1'
-    END AS MODEL,
-    AVG(MINUTE(timediff(tp.completed,tp.started))) as CT,
-    COUNT(MINUTE(timediff(tp.completed,tp.started))) as MOD_COUNT
-    FROM sparq.thing t
-    JOIN sparq.part p on p.id = t.partid
-    JOIN sparq.thingpath tp on tp.thingid = t.id
-    JOIN sparq.actor a on a.id = tp.actormodifiedby
-    WHERE tp.flowstepname = '3BM-52000'
-    AND tp.completed BETWEEN '{start}' and '{end}'
-    GROUP BY 1,2
-    """
-    df = pd.read_sql(ct_query,db)
-    
-    ct_df = pd.DataFrame({'LINE' : [], 'CT' : []})
-    i_ct_df = pd.DataFrame({'LINE' : [], 'I_CT' : []})
-    row = []
-    for line in line_list:
-        sub_df = df.query(f"LINE=='{line}'")
-        mod_count = 0
-        count_x_ct = 0
-        count_x_ict = 0
-        for row in sub_df.itertuples(False,'Tuples'):
-            mod_count += row.MOD_COUNT
-            ideal_ct = get_ideal_ct(ideal_ct_df,line,row.MODEL)
-            count_x_ct += row.MOD_COUNT*row.CT
-            count_x_ict += row.MOD_COUNT*ideal_ct
-        if mod_count:
-            avg_ct = round(count_x_ct/mod_count)
-            avg_i_ct = round(count_x_ict/mod_count)
-        else:
-            avg_ct = 'null'
-            avg_i_ct = 'null'
-        row = [line,avg_ct]
-        i_row = [line,avg_i_ct]
-        ct_df.loc[len(ct_df)] = row
-        i_ct_df.loc[len(i_ct_df)] = i_row
-    
-    return ct_df,i_ct_df
-
 def query_bonder_list(db):
     query = f"""
             SELECT 
@@ -138,26 +71,6 @@ def query_first_log(db,start,machine_id):
     df = pd.read_sql(query,db)
     return df
     
-def get_total_time(sub_df,col_name):
-    # sub_logs_df_sub = sub_df.query(f"{col_name}==1")
-    sub_logs_df_sub = sub_df[sub_df[col_name].astype(int)==1]
-    if len(sub_logs_df_sub):
-        total_time = int(round(sub_logs_df_sub.groupby(col_name)['CT_SEC'].sum().iloc[0],0))
-    else:
-        total_time = 0
-    
-    return total_time
-
-#parse dataframes for line-based value
-def get_teep_val(df,line,quad,u_or_a):
-    if len(df):
-        col = 'UTILIZATION' if u_or_a == 'U' else 'AVAILABILITY'
-        sub_df = df.query(f"LINE=={line} and QUAD=='{quad}'")
-        val = sub_df.iloc[0][col] if len(sub_df) else 0
-    else:
-        val = 0
-    return val
-
 def get_mttr_table(db,start,end):
     bonder_start = start+timedelta(hours=-2)
     BONDTOOL_FAULT_CODE = 214
@@ -431,7 +344,7 @@ def get_starved_table(db,start,end):
     ing1_starved = round(helper_functions.get_val(ing_df,'3BM1','LINE','Duration')/seconds_between*100,1)
     ing2_starved = round(helper_functions.get_val(ing_df,'3BM2','LINE','Duration')/seconds_between*100,1)
     ing3_starved = round(helper_functions.get_val(ing_df,'3BM3','LINE','Duration')/seconds_between*100,1)
-    ing4_starved = round(helper_functions.get_val(ing_df,'3BM4','LINE','Duration')/seconds_between*100,1)
+    ing4_starved = round(helper_functions.get_val(ing_df,'3BM4','LINE','Duration')/seconds_between*100/3,1)
     ing5_starved = round(helper_functions.get_val(ing_df,'3BM5','LINE','Duration')/seconds_between*100,1)
 
     po1_starved = round(helper_functions.get_val(po_df,'3BM1','LINE','Duration')/seconds_between*100,1)
@@ -527,7 +440,7 @@ def main(env,eos=False):
 
     output_html = "<table>" + "<caption>Throughput</caption>" + output_table + "</table>"
     starved_html = "<table>" + "<caption>Starvation %</caption>" + starve_table + "</table>"
-    wb_html = "<table>" + "<caption>Consumable Change Lost Minutes</caption>" +  mttr_table + "</table>"
+    wb_html = "<table>" + "<caption>Consumable Change Overtime (minutes)</caption>" +  mttr_table + "</table>"
 
     # wb_teep_html = z3_wb_teep.bonder_main(start,end)
 
