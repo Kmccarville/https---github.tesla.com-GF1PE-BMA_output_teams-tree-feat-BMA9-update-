4b3561c5157211e1dcb631be8fd522dc262c87a4
Michael Berlied <mberlied@tesla.com>
2023-02-27 14:27:28 -0800

    schedule z1 output

--- app/main.py
+++ app/main.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     #define hourly scheduler
     scheduler_hourly.every().hour.at(":00").do(output123.main,env)
     scheduler_hourly.every().hour.at(":00").do(output45.main,env)
+    scheduler_hourly.every().hour.at(":00").do(outputz1.main,env)
     scheduler_hourly.every().hour.at(":00").do(output8.main,env)
     scheduler_hourly.every().hour.at(":00").do(outputz3.main,env)
     scheduler_hourly.every().hour.at(":00").do(outputz4.main,env)
