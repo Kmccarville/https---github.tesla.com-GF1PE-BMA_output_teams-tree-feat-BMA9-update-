c040bb8ff89b6106dc01dd74b288687d71b4826c
Michael Berlied <mberlied@tesla.com>
2023-07-06 16:55:20 -0700

    python formatting and remove print

--- app/resources/alerts/z2_fixtures.py
+++ app/resources/alerts/z2_fixtures.py
@@ -11,51 +11,66 @@ import numpy as np
 def get_fixture_table():
     mos_con = helper_functions.get_sql_conn('mos_rpt2',schema='sparq')
     query = f"""
-            select LINE, TYPE,count(distinct(Carrier_ID)) as 'Count'
-            from(
-                    select  distinct td.valuetext as 'Carrier_ID',
-                    CASE WHEN actor.name IN ('3BM1-40000-01') THEN 'LINE 1'
-                    WHEN actor.name IN ('3BM2-40000-01')THEN 'LINE 2'
-                    WHEN actor.name IN ('3BM3-40000-01')THEN 'LINE 3' 
-                    WHEN actor.name in ('MMAMC2.0-LINE3-B','3BM8-42000-01', '3BM8-44000-01') THEN 'LINE 8'
-                    end as 'LINE',
-
-                    CASE WHEN parameter.name IN ('MMAMC Table ID') THEN 'NMAMC Pallet ID' 
-                    WHEN task.name in ('NIC_CARRIER') then 'NIC Clamshell Carrier'
-                    WHEN task.name in ('IC_CARRIER') then 'IC Clamshell Carrier'
-                    else parameter.name
-                    end as "TYPE"
-                    from thingdata td
-                    inner join thingpath on thingpath.thingid = td.thingid
-                    inner join parameter on parameter.id = td.parameterid
-                    inner join task on task.id = parameter.taskid
-                    inner join actor on actor.id = thingpath.actormodifiedby
-                    where td.parameterid in (select id from parameter where name in ('IC Clamshell Carrier','Picture Frame RFID','NIC Clamshell Carrier','MMAMC Table ID','Carrier_ID'))
-                    and actor.name in ('3BM1-40000-01', '3BM2-40000-01', '3BM3-40000-01', 'MMAMC2.0-LINE3-B')
-                    and td.created > now()- interval 3 hour
-
-                    UNION
-
-                    select
-                    distinct td.valuetext as 'Carrier_ID',
-                    CASE WHEN actor.name IN ('3BM1-29500-01','3BM1-29400-01') THEN 'LINE 1'
-                    WHEN actor.name IN ('3BM2-29500-01','3BM2-29400-01')THEN 'LINE 2'
-                    WHEN actor.name IN ('3BM3-29500-01','3BM3-29400-01')THEN 'LINE 3' end as 'LINE',
-                    parameter.name as "TYPE"
-                    from thingdata td
-                    inner join thingpath on thingpath.thingid = td.thingid
-                    inner join parameter on parameter.id = td.parameterid
-                    inner join actor on actor.id = thingpath.modifiedby
-                    where td.parameterid in (select id from parameter where name in ('NMAMC Pallet ID'))
-                    and actor.name in ('3BM1-29400-01', '3BM2-29400-01', '3BM3-29400-01')
-                    and td.created > now()- interval 3 hour
-                    ) sub
-            GROUP BY 1,2
-            ORDER BY 1,2
+            SELECT 
+                LINE, TYPE, COUNT(DISTINCT (Carrier_ID)) AS 'Count'
+            FROM
+                (SELECT DISTINCT
+                    td.valuetext AS 'Carrier_ID',
+                        CASE
+                            WHEN actor.name IN ('3BM1-40000-01') THEN 'LINE 1'
+                            WHEN actor.name IN ('3BM2-40000-01') THEN 'LINE 2'
+                            WHEN actor.name IN ('3BM3-40000-01') THEN 'LINE 3'
+                            WHEN actor.name IN ('MMAMC2.0-LINE3-B' , '3BM8-42000-01', '3BM8-44000-01') THEN 'LINE 8'
+                        END AS 'LINE',
+                        CASE
+                            WHEN parameter.name IN ('MMAMC Table ID') THEN 'NMAMC Pallet ID'
+                            WHEN task.name IN ('NIC_CARRIER') THEN 'NIC Clamshell Carrier'
+                            WHEN task.name IN ('IC_CARRIER') THEN 'IC Clamshell Carrier'
+                            ELSE parameter.name
+                        END AS 'TYPE'
+                FROM
+                    thingdata td
+                INNER JOIN thingpath ON thingpath.thingid = td.thingid
+                INNER JOIN parameter ON parameter.id = td.parameterid
+                INNER JOIN task ON task.id = parameter.taskid
+                INNER JOIN actor ON actor.id = thingpath.actormodifiedby
+                WHERE
+                    td.parameterid IN (SELECT 
+                            id
+                        FROM
+                            parameter
+                        WHERE
+                            name IN ('IC Clamshell Carrier' , 'Picture Frame RFID', 'NIC Clamshell Carrier', 'MMAMC Table ID', 'Carrier_ID'))
+                        AND actor.name IN ('3BM1-40000-01' , '3BM2-40000-01', '3BM3-40000-01', 'MMAMC2.0-LINE3-B')
+                        AND td.created > NOW() - INTERVAL 3 HOUR UNION SELECT DISTINCT
+                    td.valuetext AS 'Carrier_ID',
+                        CASE
+                            WHEN actor.name IN ('3BM1-29500-01' , '3BM1-29400-01') THEN 'LINE 1'
+                            WHEN actor.name IN ('3BM2-29500-01' , '3BM2-29400-01') THEN 'LINE 2'
+                            WHEN actor.name IN ('3BM3-29500-01' , '3BM3-29400-01') THEN 'LINE 3'
+                        END AS 'LINE',
+                        parameter.name AS 'TYPE'
+                FROM
+                    thingdata td
+                INNER JOIN thingpath ON thingpath.thingid = td.thingid
+                INNER JOIN parameter ON parameter.id = td.parameterid
+                INNER JOIN actor ON actor.id = thingpath.modifiedby
+                WHERE
+                    td.parameterid IN (SELECT 
+                            id
+                        FROM
+                            parameter
+                        WHERE
+                            name IN ('NMAMC Pallet ID'))
+                        AND actor.name IN ('3BM1-29400-01' , '3BM2-29400-01', '3BM3-29400-01')
+                        AND td.created > NOW() - INTERVAL 3 HOUR) sub
+            GROUP BY 1 , 2
+            ORDER BY 1 , 2
             """
     # get df
     df = pd.read_sql(text(query), mos_con)
     df = df.astype(str)
+    mos_con.close()
     # replace mos parameter to common abbreviation
     df = df.replace('IC Clamshell Carrier','IC Carrier')
     df = df.replace('NIC Clamshell Carrier','NIC Carrier')
@@ -72,8 +87,7 @@ def get_fixture_table():
     # define goal column
     goals = [27,27,62,8]
     df['GOAL'] = goals
-    print(df)
-    mos_con.close()
+
     return df
 
 def main(env):
