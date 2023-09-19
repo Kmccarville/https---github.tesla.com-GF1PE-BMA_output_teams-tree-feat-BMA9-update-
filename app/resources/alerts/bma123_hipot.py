6cda699c2842154bc0274776213aecca74b59e7a
Devin Klein <deklein@tesla.com>
2023-09-19 14:03:59 -0700

    fixed funciton ref in main

--- app/resources/alerts/bma123_hipot.py
+++ app/resources/alerts/bma123_hipot.py
@@ -80,7 +80,7 @@ def main(env):
 
         logging.info("BMA123 Hipot Alert %s" % datetime.utcnow())
 
-        df = get_fixture_table()
+        df = get_hipot_table()
         webhook_key = 'teams_webhook_BMA123_OCAP_Alerts' if env=='prod' else 'teams_webhook_DEV_Updates'
         title = 'BMA123 Z2 Hipot Alert'
         caption = 'Count Last 3 Hours'
