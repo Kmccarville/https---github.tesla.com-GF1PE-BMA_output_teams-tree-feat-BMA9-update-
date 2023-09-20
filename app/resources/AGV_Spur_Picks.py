00358e4aaa5bbe4f4a0951b2d89e4d6b26ab19b0
Nithy Shanmugam <nshanmugam@tesla.com>
2023-09-20 11:19:08 -0700

    Update AGV_Spur_Picks.py

--- app/resources/AGV_Spur_Picks.py
+++ app/resources/AGV_Spur_Picks.py
@@ -100,7 +100,7 @@ WHERE
                 </table>"""
     
     webhook = 'teams_webhook_AGV_NCM_Spur_Update' if env == 'prod' else 'teams_webhook_DEV_Updates'
-    creds = helper_functions.get_pw_json('webhook')
+    creds = helper_functions.get_pw_json(webhook)
     webhookURL = creds['url'] 
     msg_title = 'AGV Spur Update'
     msg_summary = "Hourly Update"
