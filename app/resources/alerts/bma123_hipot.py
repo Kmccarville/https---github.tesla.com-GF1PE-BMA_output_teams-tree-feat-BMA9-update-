dcce596d0bc5eab19e784254a1c950f465c695eb
Devin Klein <deklein@tesla.com>
2023-09-25 15:39:25 -0700

    test

--- app/resources/alerts/bma123_hipot.py
+++ app/resources/alerts/bma123_hipot.py
@@ -67,7 +67,7 @@ def get_hipot_table():
     df = df.sort_values('LINE')
 
     #df[['ACW1', 'ACW2', 'ACW3', 'ACW4', 'ACW5', 'ACW6', 'ACW7', 'DCW1', 'DCW2', 'DCW3', 'DCW4', 'DCW5', 'DCW6', 'DCW7']] = df[['ACW1', 'ACW2','ACW3', 'ACW4','ACW5', 'ACW6','ACW7', 'DCW1','DCW2', 'DCW3', 'DCW4', 'DCW5', 'DCW6', 'DCW7']].apply(pd.to_numeric, errors='coerce')
-    df[['PARAMETER']] = df[['PARAMETER']].apply(pd.to_numeric, errors='coerce')
+    df[['BAD']] = df[['BAD']].apply(pd.to_numeric, errors='coerce')
 
     return df
 
