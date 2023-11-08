bcc7fa81fb1a82daf734d6a4f260e07b2916e904
Michael Berlied <mberlied@tesla.com>
2023-11-07 16:14:22 -0800

    bugfix NCM

--- app/resources/NCM_bandolier_milan_output.py
+++ app/resources/NCM_bandolier_milan_output.py
@@ -9,62 +9,62 @@ from datetime import datetime
 from datetime import timedelta
 
 def sendTeamsMessage(webhook, title, summary, message,color='#cc0000'):
-	teams_msg = pymsteams.connectorcard(webhook)
-	teams_msg.title(title)
-	teams_msg.summary(summary)
-	teams_msg.color(color)
+    teams_msg = pymsteams.connectorcard(webhook)
+    teams_msg.title(title)
+    teams_msg.summary(summary)
+    teams_msg.color(color)
 
-	# create cards for each major html
-	output_card = pymsteams.cardsection()
-	output_card.text(message)
-	teams_msg.addSection(output_card)
+    # create cards for each major html
+    output_card = pymsteams.cardsection()
+    output_card.text(message)
+    teams_msg.addSection(output_card)
 
-	# SEND text to Teams
-	teams_msg.send()
-	
-def main(env,lookback):
-	lookback=12 if eos else 1
-    	now=datetime.utcnow()
-    	now_sub1hr=now+timedelta(hours=-lookback)
-    	start=now_sub1hr.replace(minute=00,second=00,microsecond=00)
-    	end=start+timedelta(hours=lookback)
-	
-	query = f"""SELECT DISTINCT
-	t.created 'Bando_Created',
-	t.name 'Bando_Serial',
-	CONVERT_TZ(tp.exited, 'UTC', 'US/Pacific') 'Transaction_Time',
-	tp.exitcompletioncode 'Exit_Reason',
-	tp.flowstepname 'Flowstep_Name',
-	a.name 'Actor'
+    # SEND text to Teams
+    teams_msg.send()
+    
+def main(env,eos=False):
+    lookback=12 if eos else 1
+    now=datetime.utcnow()
+    now_sub1hr=now+timedelta(hours=-lookback)
+    start=now_sub1hr.replace(minute=00,second=00,microsecond=00)
+    end=start+timedelta(hours=lookback)
+    
+    query = f"""SELECT DISTINCT
+    t.created 'Bando_Created',
+    t.name 'Bando_Serial',
+    CONVERT_TZ(tp.exited, 'UTC', 'US/Pacific') 'Transaction_Time',
+    tp.exitcompletioncode 'Exit_Reason',
+    tp.flowstepname 'Flowstep_Name',
+    a.name 'Actor'
 FROM
-	thingpath tp
-		INNER JOIN
-	thing t ON t.id = tp.thingid
-		INNER JOIN
-	actor a ON a.id = t.modifiedby
+    thingpath tp
+        INNER JOIN
+    thing t ON t.id = tp.thingid
+        INNER JOIN
+    actor a ON a.id = t.modifiedby
 WHERE
-	tp.flowstepname = '3BM-20104-NCM'
-		AND tp.iscurrent = 0
-		AND tp.exited > NOW() - INTERVAL {lookback} HOUR 
-		AND tp.exitcompletioncode = 'PASS'
-		AND a.name = 'Bando-Rework-Milan-01'"""
-	
-	db = helper_functions.get_sql_conn('mos_rpt2',schema="sparq")
-	df = pd.read_sql(query,db)
-	db.close()
-	u1=df['Bando_Serial'].nunique()
-	
-	message = f"""<html>
-			<tr>
-				<td <td> Milan Output for the day : </td>
-				<td <td style="color: #0000ff" > {u1}</td>
-				<td <td> bandoliers </td>
-			</tr>
-			</html>"""
-		
-	webhook = 'teams_webhook_NCM_Bando_Milan_Update' if env == 'prod' else 'teams_webhook_DEV_Updates'
-	creds = helper_functions.get_pw_json(webhook)
-	webhookURL = creds['url'] 
-	msg_title = 'NCM - Milan Update'
-	msg_summary = "Daily Update"
-	sendTeamsMessage(webhookURL,msg_title,msg_summary,message)
+    tp.flowstepname = '3BM-20104-NCM'
+        AND tp.iscurrent = 0
+        AND tp.exited > NOW() - INTERVAL {lookback} HOUR 
+        AND tp.exitcompletioncode = 'PASS'
+        AND a.name = 'Bando-Rework-Milan-01'"""
+    
+    db = helper_functions.get_sql_conn('mos_rpt2',schema="sparq")
+    df = pd.read_sql(query,db)
+    db.close()
+    u1=df['Bando_Serial'].nunique()
+    
+    message = f"""<html>
+            <tr>
+                <td <td> Milan Output for the day : </td>
+                <td <td style="color: #0000ff" > {u1}</td>
+                <td <td> bandoliers </td>
+            </tr>
+            </html>"""
+        
+    webhook = 'teams_webhook_NCM_Bando_Milan_Update' if env == 'prod' else 'teams_webhook_DEV_Updates'
+    creds = helper_functions.get_pw_json(webhook)
+    webhookURL = creds['url'] 
+    msg_title = 'NCM - Milan Update'
+    msg_summary = "Daily Update"
+    sendTeamsMessage(webhookURL,msg_title,msg_summary,message)
