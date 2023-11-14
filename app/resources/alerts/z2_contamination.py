08ef7d21e35505bd16bfc4bf0643b77a090b516c
Mackenzie Tolliver <matolliver@tesla.com>
2023-11-14 13:27:11 -0800

    update to output channel

--- app/resources/alerts/z2_contamination.py
+++ app/resources/alerts/z2_contamination.py
@@ -111,7 +111,7 @@ def main(env, threshold_count = 0):
         message = "<table>" + "<caption>Line and List of Serials</caption>" + header_html + content_html + "</table>"
         webhook_key = 'teams_webhook_BMA123_Updates' if env=='prod' else 'teams_webhook_DEV_Updates'
         webhook_json = helper_functions.get_pw_json(webhook_key)
-        webhook = webhook_json['url']
+        webhook = webhook_json['https://teslamotorsinc.webhook.office.com/webhookb2/54f28a8e-1000-4e91-8013-4118066f542b@9026c5f4-86d0-4b9f-bd39-b7d4d0fb4674/IncomingWebhook/bdd07e4970c640b9b88b406f423ce60d/ee5aa1bc-2191-4554-9fd1-ab785e58c0d5']
         
         #making the hourly teams message
         teams_msg = pymsteams.connectorcard(webhook)
