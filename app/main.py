e6819a4918c3cc85c743215ec4b32f18c79f2c5d
Michael Berlied <mberlied@tesla.com>
2023-11-15 10:12:06 -0800

    remove acta passdown from schedule

--- app/main.py
+++ app/main.py
@@ -60,13 +60,8 @@ if __name__ == '__main__':
     scheduler_alerts.every().hour.at(":00").do(z2_fixtures.main,env)
     scheduler_alerts.every().hour.at(":00").do(bma123_hipot.main,env)
     scheduler_alerts.every().hour.at(":00").do(bma123_c3a_dispense.main,env)
-    
     scheduler_alerts.every().day.at("02:00").do(NCM_bandolier_milan_output.main,env)
 
-    #define passdown scheduler
-    scheduler_passdown.every().day.at("15:30").do(cta123_eqt_email.main,env)
-    scheduler_passdown.every().day.at("03:30").do(cta123_eqt_email.main,env)
-    
     #define staffing scheduler
     scheduler_passdown.every().day.at("14:35").do(staffing.main,env)
     scheduler_passdown.every().day.at("02:35").do(staffing.main,env)
