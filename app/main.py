e4b77dc507d579ff9973f9ac69aaae0c2350091a
Mackenzie Tolliver <matolliver@tesla.com>
2023-12-16 23:42:06 -0800

    test

--- app/main.py
+++ app/main.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
         logging.info("Run all command executed")
         scheduler_hourly.run_all(delay_seconds=10)
         scheduler_alerts.run_all(delay_seconds=10)
-        scheduler_passdown.run_all(delay_seconds=10)
+        scheduler_passdown.run_all(delay_seconds=10))
         devHeading.main(start=False)
         logging.info("Run all command complete. Quiting Program")
         quit()
