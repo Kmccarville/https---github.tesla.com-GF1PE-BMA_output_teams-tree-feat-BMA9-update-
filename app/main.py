955ba37da378bf2168e34d11981fa028e8708f0d
Aidan Vancil [I] <avancil@tesla.com>
2024-03-13 12:29:48 -0700

    docs: better logging / descriptive names.
    Credit: Marion for finding the logging issue being the misfire_grace_time. :thumbsup:
    Co-Authored-By: Marion Quien <2995+mquien@users.noreply.github.tesla.com>

--- app/main.py
+++ app/main.py
@@ -33,26 +33,27 @@ if __name__ == '__main__':
     
     scheduler = BlockingScheduler()
     job_list = []
+
     #define hourly scheduler
-    job_list.append(scheduler.add_job(outputz1.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id='outputz1'))
-    job_list.append(scheduler.add_job(outputz2_123.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id='outputz2_123'))
-    job_list.append(scheduler.add_job(outputz2_45.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="outputz2_45"))
-    job_list.append(scheduler.add_job(outputz2_8.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="outputz2_8"))
-    job_list.append(scheduler.add_job(outputz3.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="outputz3"))
-    job_list.append(scheduler.add_job(outputz4.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="outputz4"))
-    job_list.append(scheduler.add_job(close_nc_check.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="close_nc_check"))
-    job_list.append(scheduler.add_job(eos.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="eos"))
-    job_list.append(scheduler.add_job(AGV_Spur_Picks.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="AGV_Spur_Picks"))
-    job_list.append(scheduler.add_job(NCM_bandolier_milan_output.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="NCM_bandolier_milan_output"))
+    job_list.append(scheduler.add_job(outputz1.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name='outputz1', misfire_grace_time=None))
+    job_list.append(scheduler.add_job(outputz2_123.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name='outputz2_123', misfire_grace_time=None))
+    job_list.append(scheduler.add_job(outputz2_45.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="outputz2_45", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(outputz2_8.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="outputz2_8", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(outputz3.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="outputz3", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(outputz4.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="outputz4", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(close_nc_check.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="close_nc_check", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(eos.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="eos", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(AGV_Spur_Picks.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="AGV_Spur_Picks", misfire_grace_time=None))
+    job_list.append(scheduler.add_job(NCM_bandolier_milan_output.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="NCM_bandolier_milan_output", misfire_grace_time=None))
 
     #define alert scheduler
-    job_list.append(scheduler.add_job(z2_contamination.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="z2_contamination"))
-    job_list.append(scheduler.add_job(z2_fixtures.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="z2_fixtures"))
-    job_list.append(scheduler.add_job(bma123_hipot.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="bma123_hipot"))
-    job_list.append(scheduler.add_job(bma123_c3a_dispense.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], id="bma123_c3a_dispense"))
+    job_list.append(scheduler.add_job(z2_contamination.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="z2_contamination"))
+    job_list.append(scheduler.add_job(z2_fixtures.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="z2_fixtures"))
+    job_list.append(scheduler.add_job(bma123_hipot.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="bma123_hipot"))
+    job_list.append(scheduler.add_job(bma123_c3a_dispense.main, CronTrigger.from_crontab('0 * * * *', PST_TZ), args=[env], name="bma123_c3a_dispense"))
 
     #define staffing scheduler
-    job_list.append(scheduler.add_job(staffing.main, CronTrigger.from_crontab('35 6,18 * * *', PST_TZ), args=[env], id="staffing"))
+    job_list.append(scheduler.add_job(staffing.main, CronTrigger.from_crontab('35 6,18 * * *', PST_TZ), args=[env], name="staffing"))
     
     #error handling
     scheduler.add_listener(listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
